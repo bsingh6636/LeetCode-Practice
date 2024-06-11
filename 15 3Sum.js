bdc34ef3e1bb153d12d56e0f42833519c7f4e00d
Brijesh Singh <bsingh6636@gmail.com>
2024-06-11 22:41:40 +0530

    3 sum

---
+++ 15 3Sum.js
@@ -0,0 +1,30 @@
+var threeSum = function (nums) {
+	if (nums.length==0 ) return ""
+	let result = []
+	nums.sort((a, b) => a - b);
+	
+	for (let i = 0; i < nums.length - 2; i++) {
+		if (i > 0 && nums[i] == nums[i - 1]) continue;
+		let j = i + 1
+		let k = nums.length - 1
+		while (j < k) {
+			let sum = nums[i] + nums[j] + nums[k]
+			if (sum == 0) {
+				result.push([nums[i], nums[j], nums[k]])
+				while (nums[j] == nums[j + 1]) j++;
+				while (nums[k] == nums[k - 1]) k--;
+				j++
+				k--
+			} else if (sum < 0)	j++;
+			 else 	k--;
+			}
+	}
+	return result
+};
+nums = [-1, 0, 1, 2, -1, -4]
+num = [0, 5, 10, -10, -2, -3]
+console.log(threeSum([5, -3, 0, -2]))
+console.log(threeSum([-1,0,1,2,-1,-4]))
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+console.log(threeSum([-1,0,1,2,-1,-4]))
+
