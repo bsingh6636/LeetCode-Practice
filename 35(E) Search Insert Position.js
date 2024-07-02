b70b3b12e9c42a672b12e80f1b500b60b99c262f
Brijesh Singh <bsingh6636@gmail.com>
2024-07-03 00:30:08 +0530

    leetcode 35

--- 35(E) Search Insert Position.js
+++ 35(E) Search Insert Position.js
@@ -1,13 +1,8 @@
 // 35. Search Insert Position
-// Easy
-// Topics
-// Companies
+
 // Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
 // You must write an algorithm with O(log n) runtime complexity.
-
- 
-
 // Example 1:
 
 // Input: nums = [1,3,5,6], target = 5
@@ -27,10 +22,9 @@ var searchInsert = function(nums, target) {
             console.log(i)
             return i;
         }
-        //  else if(nums[i]>target){
-        //     return i
-        // }
+       
     }
+    return nums.length //if the number is placed to be end of the array
 };
 
 // console.log(searchInsert([1,3,5,6],5))
