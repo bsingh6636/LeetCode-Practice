e5cfb2f33fbc2d6e9c00350a21777579727b6123
Brijesh Singh <bsingh6636@gmail.com>
2024-07-18 00:03:04 +0530

    leetCode 67 and 2

---
+++ temp.js
@@ -0,0 +1,18 @@
+// 67. Add Binary
+// Given two binary strings a and b, return their sum as a binary string.
+
+// Example 1:
+// Input: a = "11", b = "1"
+// Output: "100"
+
+// Example 2:
+// Input: a = "1010", b = "1011"
+// Output: "10101"
+
+// var addBinary = function(a, b) {
+//     return (a.toString(2)+b.toString(2))
+// };
+
+// console.log(addBinary(11,1))
+// let a=3
+// console.log(a.toString(2))
