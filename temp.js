f754c558b8b65a6bc67bb898f639d4a18cb15ccd
Brijesh Singh <bsingh6636@gmail.com>
2024-07-28 18:30:05 +0530

    problem 11

--- temp.js
+++ temp.js
@@ -1,7 +1,7 @@
 // 67. Add Binary
 // Given two binary strings a and b, return their sum as a binary string.
 
-// Example 1:
+// Example 1:https://leetcode.com/problems/container-with-most-water/
 // Input: a = "11", b = "1"
 // Output: "100"
 
@@ -16,3 +16,30 @@
 // console.log(addBinary(11,1))
 // let a=3
 // console.log(a.toString(2))
+
+// 7. Reverse Integer
+
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+// Example 1:
+
+// Input: x = 123
+// Output: 321
+// Example 2:
+
+// Input: x = -123
+// Output: -321
+// Example 3:
+
+// Input: x = 120
+// Output: 21
+
+var reverse = function(x) {
+    
+};
+
+console.log(reverse(123))
